786e428bce5edd4f076cd0a4b750f5a78ce061cc
Amal-Dahshan <amaldahshan11710@gmail.com>
2024-02-23 13:52:24 -0800

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,17 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+        Retrieves an object based on the class name and its ID, or
+        None if not found
+        """
+        if cls is in classes.values():
+            all_cls = models.storage.all(cls)
+            for value in all_cls.values():
+                if (value.id == id):
+                    return value
+
+        else:
+            return None
