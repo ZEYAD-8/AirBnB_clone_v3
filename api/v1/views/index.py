1035ce482cc29c717d47a6d8b3dbe243170e3c04
Amal-Dahshan <amaldahshan11710@gmail.com>
2024-02-26 10:48:43 -0800

    task4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,3 +14,14 @@ def status():
     status = {"status": "OK"}
     return jsonify(status)
 
+@app_views.route("/stats", strict_slashes=False)
+def num_obj():
+    """retrieves the number of each object by type"""
+    return jsonify({
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Place),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
+    })
