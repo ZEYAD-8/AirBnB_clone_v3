f908341e38b21d8d65307862439471f8ce75e16a
Amal-Dahshan <amaldahshan11710@gmail.com>
2024-02-27 05:10:16 -0800

    test module

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3 cities.py places.py places_amenities.py places_reviews.py states.py users.py
+"""Add view for object""" cities.py places.py places_amenities.py places_reviews.py states.py users.py
